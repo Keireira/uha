70b1fc4d0512abab7e072b3cad0fb99741a7d884
Aljóna Đukić <github@alena.red>
2026-02-03 16:47:25 +0500

    fix: 🗃️ merge migrations

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,16 +1,12 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
-
 import journal from './meta/_journal.json';
-import m0000 from './0000_pink_shaman.sql';
-import m0001 from './0001_lying_eternals.sql';
-import m0002 from './0002_tiresome_mandarin.sql';
+import m0000 from './0000_needy_tarot.sql';
+import m0001 from './0001_overjoyed_silvermane.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001,
-m0002
+m0001
     }
   }
   
