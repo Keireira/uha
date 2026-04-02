e7b16c84f9482790408ccdee8392a21c1e22070d
Aljóna Đukić <github@alena.red>
2026-04-02 23:15:00 +0500

    add api endpoint handlers

--- src/api/soup/soup.ts
+++ src/api/soup/soup.ts
@@ -1,5 +1,31 @@
-// import * as Crypto from 'expo-crypto';
+import * as Crypto from 'expo-crypto';
 
-// import { soupApi } from '@api/api';
+import { soupApi } from '@api/api';
 
-// import type { InitQueryT, InitResponseT } from './soup.d';
+import type { SearchQueryT, SearchResponseT, ServiceResponseT } from './soup.d';
+
+export const searchService = (query: SearchQueryT['q'], sources?: SearchQueryT['sources']) => {
+	const queryParams: SearchQueryT = {
+		q: query,
+		sources: sources ?? 'all'
+	};
+
+	const request = soupApi<SearchResponseT>('/search', {
+		traceId: Crypto.randomUUID(),
+		method: 'get',
+		params: {
+			queryParams
+		}
+	});
+
+	return request satisfies Promise<SearchResponseT>;
+};
+
+export const getService = (serviceId: string) => {
+	const request = soupApi<ServiceResponseT>(`/service/${serviceId}`, {
+		traceId: Crypto.randomUUID(),
+		method: 'get'
+	});
+
+	return request satisfies Promise<ServiceResponseT>;
+};
