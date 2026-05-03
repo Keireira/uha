afab24b607dfdae20e775707a94062460da9fa20
Aljóna Đukić <github@alena.red>
2026-05-03 23:18:36 +0500

    fix: ♻️ search results

--- src/api/soup/search.ts
+++ src/api/soup/search.ts
@@ -1,56 +1,115 @@
 import * as Crypto from 'expo-crypto';
+
 import { soupApi } from '@api/api';
 import { searchAppStore } from './appstore';
+import { normalizeDomain } from '@lib/url';
+
 import type { SearchOptions, SearchQueryT, SearchResponseT, SearchResultT } from './soup.d';
 
-const normalizeDomain = (d: string) => d.toLowerCase().replace(/^www\./, '');
+/* Response handling */
+const isKnown = (searchResult: SearchResultT, knownDomains: Set<string>) => {
+	const result = searchResult.domains.some((url) => {
+		const normalized = normalizeDomain(url);
 
-const deduplicate = (fromSoup: SearchResultT[], appstore: SearchResultT[]): SearchResultT[] => {
+		return knownDomains.has(normalized);
+	});
+
+	return result;
+};
+
+const getKnownDomains = (soupResults: SearchResultT[]) => {
+	const knownDomains = new Set<string>();
+
+	for (const result of soupResults) {
+		for (const domain of result.domains) {
+			knownDomains.add(normalizeDomain(domain));
+		}
+	}
+
+	return knownDomains;
+};
+
+const deduplicate = (soupResults: SearchResultT[], appstoreResults: SearchResultT[]) => {
 	const inhouse: SearchResultT[] = [];
 	const restResults: SearchResultT[] = [];
 
-	const knownDomains = new Set(fromSoup.flatMap((r) => r.domains.map(normalizeDomain)));
-	const isKnown = (r: SearchResultT) => r.domains.some((d) => knownDomains.has(normalizeDomain(d)));
-	const newFromAppStore = appstore.filter((r) => !isKnown(r));
+	const knownDomains = getKnownDomains(soupResults);
+	const newFromAppStore = appstoreResults.filter((result) => !isKnown(result, knownDomains));
 
-	for (const r of fromSoup) {
-		(r.source === 'inhouse' ? inhouse : restResults).push(r);
+	for (const result of soupResults) {
+		if (result.source === 'inhouse') {
+			inhouse.push(result);
+		} else {
+			restResults.push(result);
+		}
 	}
 
 	return [...inhouse, ...newFromAppStore, ...restResults];
 };
 
+/* Request handling */
 const prepareSources = (sources?: SearchOptions['sources']) => {
-	const soupSources = sources ? sources.filter((s) => s !== 'appstore').join(',') : 'all';
+	const filteredSoupSources = sources?.filter((source) => source !== 'appstore') || [];
 
 	return {
-		soupSources,
-		withAppStore: !sources || sources.includes('appstore')
+		withSoup: Boolean(filteredSoupSources.length),
+		withAppStore: sources?.includes('appstore') ?? false,
+		withGooglePlay: sources?.includes('playstore') ?? false,
+
+		soupSources: sources ? filteredSoupSources.join(',') : 'all'
 	};
 };
 
-const searchService = async (query: SearchQueryT['q'], options?: SearchOptions): Promise<SearchResponseT> => {
-	const { withAppStore, soupSources } = prepareSources(options?.sources);
+const prepareRequests = (query: SearchQueryT['q'], options?: SearchOptions) => {
+	const { withAppStore, withGooglePlay, withSoup, soupSources } = prepareSources(options?.sources);
 
-	const queryParams: SearchQueryT = {
-		q: query,
-		sources: soupSources,
-		language: options?.language,
-		playstore_country: options?.playstore_country
+	const queryParams: Partial<SearchQueryT> = {
+		q: query
 	};
 
-	const [soupResults, appStoreResults] = await Promise.all([
-		soupApi<SearchResponseT>('/search', {
-			traceId: Crypto.randomUUID(),
-			method: 'get',
-			params: {
-				queryParams
-			}
-		}),
-		withAppStore ? searchAppStore(query, options?.app_store_country) : null
-	]);
+	if (withSoup) {
+		queryParams.sources = soupSources;
+	}
 
-	return appStoreResults ? deduplicate(soupResults, appStoreResults) : soupResults;
+	if (withGooglePlay && options) {
+		queryParams.language = options.language;
+		queryParams.playstore_country = options.playstore_country;
+	}
+
+	const soupRequest = withSoup
+		? soupApi<SearchResponseT>('/search', {
+				traceId: Crypto.randomUUID(),
+				method: 'get',
+				params: { queryParams }
+			})
+		: null;
+
+	const appStoreRequest = withAppStore ? searchAppStore(query, options?.app_store_country) : null;
+
+	return [soupRequest, appStoreRequest];
+};
+
+/* Master function */
+const searchService = async (query: SearchQueryT['q'], options?: SearchOptions): Promise<SearchResponseT> => {
+	const requests = prepareRequests(query, options);
+	const [soupResult, appStoreResult] = await Promise.allSettled(requests);
+
+	const soupResults = soupResult.status === 'fulfilled' ? soupResult.value : null;
+	const appStoreResults = appStoreResult.status === 'fulfilled' ? appStoreResult.value : null;
+
+	if (soupResults && appStoreResults) {
+		return deduplicate(soupResults, appStoreResults);
+	}
+
+	if (soupResults) {
+		return soupResults;
+	}
+
+	if (appStoreResults) {
+		return appStoreResults;
+	}
+
+	throw new Error('Search failed');
 };
 
 export default searchService;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ export * from './language';
 export * from './colors';
 export * from './price';
 export * from './date';
+export * from './url';
 
 export { default as first } from './first';
 export { default as randomInt } from './random-int';
